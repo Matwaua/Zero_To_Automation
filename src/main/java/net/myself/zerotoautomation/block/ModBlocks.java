5688e5995605b7842e53a132851de573f6db5a09
matwaua <matheusferreirawa@gmail.com>
2024-10-11 11:47:02 -0400

    first version

--- src/main/java/net/myself/zerotoautomation/block/ModBlocks.java
+++ src/main/java/net/myself/zerotoautomation/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.GravelBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -22,7 +23,7 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIRT)));
 
     public static final RegistryObject<Block> ANDESITE_GRAVEL = registerBlock("andesite_gravel",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.GRAVEL)));
+            () -> new GravelBlock(BlockBehaviour.Properties.copy(Blocks.GRAVEL)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
